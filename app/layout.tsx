829945f83f276262e4bcdc13122690936209a81e
Amber <wdzhwish@163.com>
2023-05-25 17:40:51 +0000

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatBIT 明理",
+  description: "ChatBIT 明理",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatBIT 明理",
     statusBarStyle: "default",
   },
 };
